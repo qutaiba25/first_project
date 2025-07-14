ba8a8db420e26be1ee23fd6839325b32f7d01f73
qutaiba25 <qutaiba25>
2025-07-14 11:58:41 +0300

    update

--- src/testing_for_Automationteststore/login.java
+++ src/testing_for_Automationteststore/login.java
@@ -1,5 +1,7 @@
 package testing_for_Automationteststore;
 
+import java.util.Random;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,6 +13,8 @@ import org.testng.annotations.Test;
 public class login {
 
 	WebDriver driver = new ChromeDriver();
+	String username;
+	String passwordlogin = "qwer@1234";
 
 	String THEURL = "https://automationteststore.com/";
 
@@ -23,9 +27,11 @@ public class login {
 		driver.manage().window().maximize();
 	}
 
-	@Test
+	@Test(priority = 1, enabled = true)
 
 	public void SignUp() throws InterruptedException {
+
+		Random rand = new Random();
 
 		// WebElement firstresult =
 		// driver.findElement(By.xpath("//*[@id=\"customer_menu_top\"]/li/a"));
@@ -39,8 +45,8 @@ public class login {
 		// driver.findElement(By.name("firstname")).sendKeys("Qutaiba");
 
 		// elements
-		WebElement firstnameinput = driver.findElement(By.xpath("//input[@name='firstname']"));
-		WebElement lastnameinput = driver.findElement(By.xpath("//input[@name='lastname']"));
+		WebElement firstNameinput = driver.findElement(By.xpath("//input[@name='firstname']"));
+		WebElement lastNameinput = driver.findElement(By.xpath("//input[@name='lastname']"));
 		WebElement EMailinput = driver.findElement(By.xpath("//input[@name='email']"));
 		WebElement Telephoneinput = driver.findElement(By.xpath("//input[@name='telephone']"));
 		WebElement Faxinput = driver.findElement(By.xpath("//input[@name='fax']"));
@@ -55,21 +61,32 @@ public class login {
 		WebElement passwordInput = driver.findElement(By.id("AccountFrm_password"));
 		WebElement passwordconfirmInput = driver.findElement(By.id("AccountFrm_confirm"));
 		WebElement PrivacyPolicy = driver.findElement(By.id("AccountFrm_agree"));
+		WebElement ContinueButton = driver.findElement(By.xpath("//button[@title='Continue']"));
 
 		// data
-		String firstname = "Qutaiba";
-		String lastname = "hesham";
-		String EMail = firstname + lastname + "@gmail.com";
+		System.out.println(driver.getCurrentUrl());
+		rand.nextInt(9800);
+		String[] firstNames = { "anas", "ali", "kaled", "mamoun", "qutiba" };
+		int randomNameIndexForFirstNames = rand.nextInt(firstNames.length);
+		String randomFirstName = firstNames[randomNameIndexForFirstNames];
+
+		String lastNames[] = { "hesham", "ameen", "mahmoud", "qusai", "ahmad" };
+		int randomNameIndexForLastNames = rand.nextInt(lastNames.length);
+		String randomnLastName = lastNames[randomNameIndexForLastNames];
+
+		String EMail = randomFirstName + randomnLastName + "@gmail.com";
 		String Telephone = "0799628777";
 		String Fax = "8787";
-		String Company = "zain";
-		String Address1 = "tt55";
-		String Address2 = "tf44";
-		String City = "irbid";
+		String Company = "Zain";
+		String Address1 = "tt56";
+		String Address2 = "tf45";
+		String City = "Irbid";
+
+		// Thread.sleep(3000);
 
 		// actions
-		firstnameinput.sendKeys(firstname);
-		lastnameinput.sendKeys(lastname);
+		firstNameinput.sendKeys(randomFirstName);
+		lastNameinput.sendKeys(randomnLastName);
 		EMailinput.sendKeys(EMail);
 		Telephoneinput.sendKeys(Telephone);
 		Faxinput.sendKeys(Fax);
@@ -85,10 +102,42 @@ public class login {
 		myselectforcountry.selectByValue("108");
 		Select myselectforstate = new Select(state);
 		myselectforstate.selectByVisibleText("Al Karak");
-		loginName.sendKeys(firstname + lastname);
-		passwordInput.sendKeys("qwer@1234");
-		passwordconfirmInput.sendKeys("qwer@1234");
+		username = randomFirstName + randomnLastName;
+		loginName.sendKeys(username);
+		passwordInput.sendKeys(passwordlogin);
+		passwordconfirmInput.sendKeys(passwordlogin);
 		PrivacyPolicy.click();
+		Thread.sleep(3000);
+		ContinueButton.click();
+
+	}
+
+	@Test(priority = 2, enabled = true)
+
+	public void logout() throws InterruptedException {
+
+		Thread.sleep(2000);
+
+		WebElement Logout = driver.findElement(By.linkText("Logoff"));
+
+		Logout.click();
+
+	}
+
+	@Test(priority = 3, enabled = true)
+
+	public void Login() {
+
+		WebElement LoginorRegister = driver.findElement(By.partialLinkText("Login or re"));
+		LoginorRegister.click();
+
+		WebElement Login = driver.findElement(By.id("loginFrm_loginname"));
+		WebElement password = driver.findElement(By.id("loginFrm_password"));
+
+		Login.sendKeys(username);
+		password.sendKeys(passwordlogin);
+
+		driver.findElement(By.xpath("//button[@title='Login']")).click();
 
 	}
 
